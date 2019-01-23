94c8ad3bfa70d761c0e0e30cfbd9e8d136d19c6a
Thomas <40203272@live.napier.ac.uk>
2019-01-23 11:07:18 +0000

    Adding SanityCheck.java, this is simply a test.

---
+++ src/main/java/com/napier/sem/SanityCheck.java
@@ -0,0 +1,9 @@
+package com.napier.sem;
+
+public class SanityCheck
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Do not be alarmed, this is a test. Hello Group 2!");
+    }
+}
