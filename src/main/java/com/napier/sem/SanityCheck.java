c790ac854c42e76dec1cc478ca10ea021e06084b
TDiggens <40203272@live.napier.ac.uk>
2019-01-23 12:53:56 +0000

    Adding MongoDB Support to pom.xml, again, again.

--- src/main/java/com/napier/sem/SanityCheck.java
+++ src/main/java/com/napier/sem/SanityCheck.java
@@ -1,32 +1,9 @@
 package com.napier.sem;
 
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoCollection;
-import org.bson.Document;
-
 public class SanityCheck
 {
     public static void main(String[] args)
     {
         System.out.println("Do not be alarmed, this is a test. Hello Group 2!");
-
-        // Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = new MongoClient("localhost", 27000);
-        // Get a database - will create when we use it
-        MongoDatabase database = mongoClient.getDatabase("mydb");
-        // Get a collection from the database
-        MongoCollection<Document> collection = database.getCollection("test");
-        // Create a document to store
-        Document doc = new Document("name", "Kevin Chalmers")
-                .append("class", "Software Engineering Methods")
-                .append("year", "2018/19")
-                .append("result", new Document("CW", 95).append("EX", 85));
-        // Add document to collection
-        collection.insertOne(doc);
-
-        // Check document in collection
-        Document myDoc = collection.find().first();
-        System.out.println(myDoc.toJson());
     }
 }
