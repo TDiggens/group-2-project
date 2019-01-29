95984bcaeb8458ba1d9abd4da969d617d72994da
David Robertson <40293905@live.napier.ac.uk>
2019-01-29 23:28:04 +0000

    Added code to print out the countries in the data base

--- src/main/java/com/napier/sem/SanityCheck.java
+++ src/main/java/com/napier/sem/SanityCheck.java
@@ -1,9 +1,29 @@
 package com.napier.sem;
+import java.sql.*;
 
 public class SanityCheck
 {
     public static void main(String[] args)
     {
         System.out.println("Do not be alarmed, this is a test. Hello Group 2!");
-    }
+
+
+                try {
+                    Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/world", "root", "Chichi!2");
+                    Statement stmt = conn.createStatement();
+                    String selectQuery = "SELECT name FROM country";
+                    ResultSet resultSet = stmt.executeQuery(selectQuery);
+
+                    while (resultSet.next()) {
+                        String name = resultSet.getString("name");
+                        System.out.println(name);
+                    }
+                }
+                catch(SQLException ex)
+                {
+                    ex.printStackTrace();
+                }
+
+
+            }
 }
