ca504c94061d276f2bc6061600f21b7a92079113
David Robertson <40293905@live.napier.ac.uk>
2019-02-20 05:34:37 +0000

    Updated pom.xml to add MYSQL connector dependencies, updated DockerFile, added docker-compose.yml, added data folder with world.sql database

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:latest
-COPY ./target/classes/com /tmp/com
+COPY ./target/semGroup2-0.1.0.1-jar-with-dependencies.jar /tmp
 WORKDIR /tmp
-ENTRYPOINT ["java", "com.napier.sem.SanityCheck"]
+ENTRYPOINT ["java", "-jar", "semGroup2-0.1.0.1-jar-with-dependencies.jar"]
--- src/main/java/com/napier/sem/SanityCheck.java
+++ src/main/java/com/napier/sem/SanityCheck.java
@@ -10,17 +10,14 @@ public class SanityCheck
      * Connection to MySQL database.
      */
     private Connection con = null;
-    private World data;
+
 
     public static void main(String[] args)
     {
         System.out.println("Do not be alarmed, this is a test. Hello Group 2!");
         SanityCheck sanity = new SanityCheck();
-        sanity.data = new World();
-        sanity.data.setCityList(sanity.generateCityList());
-        sanity.data.setDistrictList(sanity.generateDistrictList());
-        sanity.data.setCountryList(sanity.generateCountryList());
-        sanity.calculateDistrictPops();
+        sanity.connect();
+        sanity.disconnect();
     }
 
     /**
@@ -83,103 +80,6 @@ public class SanityCheck
         }
     }
 
-    public ArrayList<City> generateCityList(){
-        ArrayList<City> cityList = new ArrayList<City>();
-        try
-        {
-            Statement stmt = con.createStatement();
-            String strSelect = "SELECT * FROM city ORDER BY population";
-            ResultSet rSet = stmt.executeQuery(strSelect);
-            while(rSet.next())
-            {
-                City city = new City();
-                city.setCountryCode(rSet.getString("CountryCode"));
-                city.setDistrict(rSet.getString("District"));
-                city.setId(rSet.getInt("ID"));
-                city.setName(rSet.getString("Name"));
-                city.setPopulation(rSet.getInt("Population"));
-                cityList.add(city);
-            }
-        }
 
-        catch(Exception e)
-        {
 
-            return null;
-        }
-        return cityList;
-    }
-
-    public ArrayList<Country> generateCountryList(){
-        ArrayList<Country> countryList = new ArrayList<Country>();
-        try
-        {
-            Statement stmt = con.createStatement();
-            String strSelect = "SELECT * FROM country ORDER BY population";
-            ResultSet rSet = stmt.executeQuery(strSelect);
-
-            while(rSet.next()) {
-                Country country = new Country();
-                country.setCapitalCode(rSet.getInt("Capital"));
-                country.setCode(rSet.getString("Code"));
-                country.setCode2(rSet.getString("Code2"));
-                country.setContinent(rSet.getString("Continent"));
-                country.setGnp(rSet.getDouble("GNP"));
-                country.setOldGNP(rSet.getDouble("GNPOld"));
-                country.setFormOfGov(rSet.getString("GovernmentForm"));
-                country.setHeadOfState(rSet.getString("HeadOfState"));
-                country.setYearOfIndependence(rSet.getInt("IndepYear"));
-                country.setLifeExpectancy(rSet.getDouble("LifeExpectancy"));
-                country.setLocalName(rSet.getString("LocalName"));
-                country.setName(rSet.getString("Name"));
-                country.setPopulation(rSet.getInt("Population"));
-                country.setRegion(rSet.getString("Region"));
-                country.setSurfaceArea(rSet.getDouble("SurfaceArea"));
-                countryList.add(country);
-            }
-        }
-        catch(Exception e)
-        {
-            System.out.println(e.getMessage());
-            return null;
-        }
-        return countryList;
-    }
-
-    public ArrayList<District> generateDistrictList(){
-        ArrayList<District> districtList = new ArrayList<District>();
-        ArrayList<City> cityList = data.getCityList();
-        boolean exists = false;
-            for(City city : cityList){
-                exists = false;
-                for(District district : districtList) {
-                    if(city.getDistrict() == district.getName()) {
-                        exists = true;
-                        if(!district.getCityList().contains(city)){
-                            district.getCityList().add(city);
-                        }
-                        break;
-                    }
-                }
-                if(!exists){
-                    District district = new District(city.getDistrict());
-                    districtList.add(district);
-                    if(!district.getCityList().contains(city)){
-                        district.getCityList().add(city);
-                    }
-                }
-            }
-        return districtList;
-    }
-
-    public void calculateDistrictPops()
-    {
-        for(District district : data.getDistrictList()){
-            int population = 0;
-            for(City city : district.getCityList()){
-                population += city.getPopulation();
-            }
-            district.setPopulation(population);
-        }
-    }
 }
