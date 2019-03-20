031875c9c4152a5c7f8de5e9e527f9f92132ab47
Thomas <40203272@live.napier.ac.uk>
2019-03-20 00:10:03 +0000

    Adding unit tests for Issue #6, Countries in Continent.

--- src/test/java/com/napier/sem/SanityCheckTest.java
+++ src/test/java/com/napier/sem/SanityCheckTest.java
@@ -15,26 +15,26 @@ public class SanityCheckTest
     {
         sanityCheck = new SanityCheck();
     }
-    //test for country argument being null
+    // test for country argument being null
     @Test
     void listCitiesInCountryNull()
     {
         sanityCheck.listCitiesInCountry(null);
     }
-    //test for the country's district list being empty.
+    // test for the country's district list being empty.
     @Test
     void listCitiesInCountryDistrictListEmpty()
     {
-        ArrayList<District>  districtList = new ArrayList<District>();
+        ArrayList<District>  districtList = new ArrayList<>();
         Country country = new Country();
         country.setDistrictList(districtList);
         sanityCheck.listCitiesInCountry(country);
     }
-    //test for the country's district list containing a null district.
+    // test for the country's district list containing a null district.
     @Test
     void listCitiesInCountryDistrictListContainsNull()
     {
-        ArrayList<District> districtList = new ArrayList<District>();
+        ArrayList<District> districtList = new ArrayList<>();
         District district = null;
         districtList.add(district);
         Country country = new Country();
@@ -45,8 +45,8 @@ public class SanityCheckTest
     @Test
     void listCitiesInCountryDistrictCityListEmpty()
     {
-        ArrayList<City> cityList = new ArrayList<City>();
-        ArrayList<District> districtList = new ArrayList<District>();
+        ArrayList<City> cityList = new ArrayList<>();
+        ArrayList<District> districtList = new ArrayList<>();
         District district = new District();
         district.setCityList(cityList);
         districtList.add(district);
@@ -55,12 +55,12 @@ public class SanityCheckTest
         sanityCheck.listCitiesInCountry(country);
     }
 
-    //Test for if one of the cities in the country's district list is null.
+    // Test for if one of the cities in the country's district list is null.
     @Test
     void listCitiesInCountryCityListContainsNull()
     {
-        ArrayList<City> cityList = new ArrayList<City>();
-        ArrayList<District> districtList = new ArrayList<District>();
+        ArrayList<City> cityList = new ArrayList<>();
+        ArrayList<District> districtList = new ArrayList<>();
         City city = null;
         cityList.add(city);
         District district = new District();
@@ -69,5 +69,60 @@ public class SanityCheckTest
         Country country = new Country();
         country.setDistrictList(districtList);
         sanityCheck.listCitiesInCountry(country);
+    }
+
+    // test for country argument being null
+    @Test
+    void listCountriesInContinentNull()
+    {
+        sanityCheck.listCountriesInContinent(null);
+    }
+    // test for the continent's region list being empty.
+    @Test
+    void listCountriesInContinentListEmpty()
+    {
+        ArrayList<Region>  regionList = new ArrayList<>();
+        Continent continent = new Continent();
+        continent.setRegionList(regionList);
+        sanityCheck.listCountriesInContinent(continent);
+    }
+    // test for the continent's region list containing a null region.
+    @Test
+    void listCountriesInContinentListContainsNull()
+    {
+        ArrayList<Region> regionList = new ArrayList<>();
+        Region region = null;
+        regionList.add(region);
+        Continent continent = new Continent();
+        continent.setRegionList(regionList);
+        sanityCheck.listCountriesInContinent(continent);
+    }
+    // test what happens when a district in the district list contains no cities
+    @Test
+    void listCountriesInContinentRegionCountryListEmpty()
+    {
+        ArrayList<Country> countryList = new ArrayList<>();
+        ArrayList<Region> regionList = new ArrayList<>();
+        Region region = new Region();
+        region.setCountryList(countryList);
+        regionList.add(region);
+        Continent continent = new Continent();
+        continent.setRegionList(regionList);
+        sanityCheck.listCountriesInContinent(continent);
+    }
+
+    // Test for if one of the countries in the continent's region list is null.
+    @Test
+    void listCountriesInContinentCountryListContainsNull() {
+        ArrayList<Country> countryList = new ArrayList<>();
+        ArrayList<Region> regionList = new ArrayList<>();
+        Country country = null;
+        countryList.add(country);
+        Region region = new Region();
+        region.setCountryList(countryList);
+        regionList.add(region);
+        Continent continent = new Continent();
+        continent.setRegionList(regionList);
+        sanityCheck.listCountriesInContinent(continent);
     }
 }
