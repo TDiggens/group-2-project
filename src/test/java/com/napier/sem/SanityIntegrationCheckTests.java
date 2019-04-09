f8f489bb51d2f60c46b979f21031edd875696881
TDiggens <40203272@live.napier.ac.uk>
2019-04-09 18:27:38 +0100

    Should work after integration tests are working. Feel free to quickly fix the one I'm trying to write and test it, I'll be afk for a wee while.

---
+++ src/test/java/com/napier/sem/SanityIntegrationCheckTests.java
@@ -0,0 +1,28 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SanityIntegrationCheckTest
+{
+    static SanityCheck sanityCheck;
+
+    @BeforeAll
+    static void init()
+    {
+        sanityCheck = new SanityCheck();
+        sanityCheck.connect("localhost:33060");
+    }
+
+    @Test
+    void testGetCountry()
+    {
+        Country country = sanityCheck.generateCountryList();
+        assertEquals(country.name, "China");
+    }
+}
